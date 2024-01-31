fe5fe73804fdf2a9830887a4b8e75a75119d1431
ADNAN <bennassar.adnan.solicode@gmail.com>
2024-01-31 13:28:35 +0100

    save

--- conception/maquette_editeur_de_rapport/resources/views/layouts/app.blade.php
+++ conception/maquette_editeur_de_rapport/resources/views/layouts/app.blade.php
@@ -7,23 +7,18 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css"> --}}
-   {{-- <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script> --}}
+   
+   
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/simplemde.min.css') }}">
 
-{{-- 
-   <link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> --}}
-   {{-- <link rel="stylesheet" href="https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css"/> --}}
-
-
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/simplemde.css') }}">
+    <script src="{{ asset('js/simplemde.min.js') }}"></script>
 
     
     @vite(['resources/css/app.css'])
 
 </head>
-<body>
+<body class="sidebar-mini layout-fixed sidebar-collapse">
     <div class="wrapper">
 
         @include('layouts.navbar')
@@ -47,8 +42,8 @@
     @vite(['resources/js/app.js'])
       
 
-   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+   {{-- <script src="https://unpkg.com/axios/dist/axios.min.js"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
